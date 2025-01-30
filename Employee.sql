dc99250185a5509454aedc1b5b83b0e5863c14a7
ConstantinosSakkas <konstantinoss2013@gmail.com>
2025-01-30 15:32:52 +0000

    Delete Employee.sql

--- Employee.sql
+++
@@ -1,8 +0,0 @@
-﻿
-INSERT INTO GLEN.Employees ( Name, Surname, Email, Department)
-VALUES 
-    ('Bob', 'Ross', 'bob.ross@something.com', 'IT'),
-    ('Rick', 'Roll', 'rick.roll@something.com', 'HR'),
-    ('Jeff', 'Strat', 'jeff.strat@something.com', 'Finance'),
-    ('Ross', 'Giller', 'ross.giller@something.com', 'Sales'),
-    ('Peter','Griffin','peter.grifin@something.com','Operations');
